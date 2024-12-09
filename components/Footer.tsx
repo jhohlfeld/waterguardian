8fbde122f15fd9b53eb83a6faebb3d08f1078dc0
oaris-dev <76177243+oaris-dev@users.noreply.github.com>
2024-12-09 17:31:26 +0100

    feat(#11): added imprint and privacy links to the footer
    - adjusted the design for the new links in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full p-4 bg-background text-gray-12 text-center">
-      <p>&copy; {new Date().getFullYear()} WaterGuardian</p>
+    <footer className="w-full py-2 px-4 bg-background text-gray-12 text-center">
+      <div className="text-sm font-medium">
+        <Link href="/privacy" className="px-2 hover:text-indigo-12">
+          Datenschutz
+        </Link>
+        <Link href="/imprint" className="px-2 hover:text-indigo-12">
+          Impressum
+        </Link>
+      </div>
+      <div className="space-x-6 mt-4 text-sm font-medium">
+        <p>&copy; {new Date().getFullYear()} WaterGuardian</p>
+      </div>
     </footer>
   )
 }
