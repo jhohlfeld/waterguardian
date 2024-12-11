adb1dc5c209fd390a13c335c7dab038a72f2d813
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2024-12-11 18:15:09 +0100

    fix(#11): fix footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,23 +2,23 @@ import Link from 'next/link'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full py-2 px-4 bg-[--gray-1] text-[--gray-12] text-center">
-      <div className="flex justify-center text-sm font-medium gap-x-1">
+    <footer className="flex flex-col gap-2 w-full p-6 pb-10 bg-[--gray-1] text-[--gray-12] text-center">
+      <div className="flex justify-center text-sm font-medium gap-4">
         <Link
           href="/privacy"
-          className="transition-all ease-in-out duration-300 p-2 hover:tracking-wide px-2 hover:text-[--indigo-11]"
+          className="transition-all ease-in-out duration-300 hover:tracking-wide hover:text-[--indigo-11]"
         >
           Datenschutz
         </Link>
         <Link
           href="/imprint"
-          className="transition-all ease-in-out duration-300 p-2 hover:tracking-wide px-2 hover:text-[--indigo-11]"
+          className="transition-all ease-in-out duration-300 hover:tracking-wide hover:text-[--indigo-11]"
         >
           Impressum
         </Link>
       </div>
-      <div className="space-x-6 mt-4 text-sm font-medium">
-        <p>&copy; {new Date().getFullYear()} WaterGuardian</p>
+      <div className="text-sm font-medium">
+        &copy; {new Date().getFullYear()} WaterGuardian
       </div>
     </footer>
   )
