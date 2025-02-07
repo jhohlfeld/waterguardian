2276961a633f7c2fc2fbe6fe61ebb241ec4f838a
oaris-dev <76177243+oaris-dev@users.noreply.github.com>
2025-02-07 19:29:21 +0100

    fix (#36): fix URL parsing issue by implementing dynamic base URL

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import { Map } from '@/components/map/Map'
 import { Waterguardian } from './api/samples/types'
 
 async function getSamples() {
-  const response = await fetch('http://localhost:3000/api/samples', {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'
+
+  const response = await fetch(`${baseUrl}/api/samples`, {
     cache: 'no-store',
   })
 
